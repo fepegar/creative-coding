160c5b56bb198e5d76a23f5ac56cfbf2efcac73c
Fernando <fepegar@gmail.com>
2021-06-18 00:15:37 +0100

    Improve squares-javi

--- squares-javi/sketch.js
+++ squares-javi/sketch.js
@@ -1,5 +1,8 @@
 var khakiWeb;
 var oxfordBlue;
+var circles = [];
+var sameInitialSpeed = true;
+
 
 function setup() {
   let minSide = min(windowWidth, windowHeight);
@@ -8,13 +11,20 @@ function setup() {
   oxfordBlue = color(12, 27, 51);
   noStroke();
   rectMode(CENTER);
-  background(oxfordBlue);
-  translate(width/2, height/2);
-  let diameter = width;
-  drawShapes(diameter);
+  randomSeed(42);
+  noiseSeed(42);
+  makeCircles();
 }
 
+function draw() {
+  background(oxfordBlue);  // draw outer square
+  translate(width/2, height/2);
+  circles.forEach(circle => {
+    circle.update();
+  });
+}
 
+// If we want to make it recursive
 function drawShapes(diameter) {
   if (diameter < 1) {
     return;
@@ -27,7 +37,7 @@ function drawShapes(diameter) {
 
 function drawCircle(diameter) {
   fill(khakiWeb);
-  ellipse(0, 0, diameter, diameter);
+  circle(0, 0, diameter);
 }
 
 function drawSquare(side) {
@@ -37,4 +47,54 @@ function drawSquare(side) {
 
 function getSquareSide(diagonal) {
   return diagonal / sqrt(2);
+}
+
+function makeCircles() {
+  let diameter = width;
+  do {
+    circ = new Circle(diameter);
+    circles.push(circ);
+    diameter = circ.square.side;
+  } while(diameter >= 1)
+}
+
+function Circle(diameter) {
+  this.diameter = diameter;
+  this.square = new Square(getSquareSide(diameter));
+
+  this.update = function() {
+    this.draw();
+    this.square.update();
+  }
+
+  this.draw = function() {
+    drawCircle(this.diameter);
+  }
+}
+
+function Square(side) {
+  this.side = side;
+  this.theta = 0;
+  this.speed = 0.001;
+
+  if (sameInitialSpeed) {
+    this.offset = 0;
+  } else {
+    this.offset = random(100000);
+  }
+
+  this.update = function() {
+    this.move();
+    this.draw();
+  }
+
+  this.move = function() {
+    let value = noise(frameCount * this.speed + this.offset);
+    this.theta = map(value, 0, 1, -TWO_PI, TWO_PI);
+  }
+
+  this.draw = function() {
+    rotate(this.theta);
+    drawSquare(this.side);
+  }
 }
